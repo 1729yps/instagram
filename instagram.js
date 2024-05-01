8d5fdd937f351d119e6cbd850a46a9f318cfa383
Pavani_Sri <158443313+1729yps@users.noreply.github.com>
2024-05-01 17:55:58 +0530

    Add files via upload

---
+++ instagram.js
@@ -0,0 +1,7 @@
+document.getElementById("instaform").addEventListener('but',function(event){
+    event.preventDefault();
+    const username=document.getElementById('username').value
+    const password=document.getElementById('password').value
+    console.log("phone number,username,or email: " + username);
+    console.log("Password: " + password);
+    })
