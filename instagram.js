7ffe2e6911a0846658c51077f90edc7bc2313687
Pavani_Sri <158443313+1729yps@users.noreply.github.com>
2024-05-01 18:20:15 +0530

    Update instagram.js

--- instagram.js
+++ instagram.js
@@ -1,7 +1,7 @@
-document.getElementById("instaform").addEventListener('but',function(event){
+document.getElementById("instaform").addEventListener('submit',function(event){
     event.preventDefault();
     const username=document.getElementById('username').value
     const password=document.getElementById('password').value
     console.log("phone number,username,or email: " + username);
     console.log("Password: " + password);
-    })
+})
